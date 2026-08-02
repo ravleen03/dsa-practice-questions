0cfb074b7767b764ecd55bf676f86dcf4ff7ebdc
ravleen03 <ravleen8360kaur06@gmail.com>
2026-08-03 02:12:41 +0530

    Time: 48 ms (21.74%) | Memory: 106 MB (36.02%) - LeetSync

--- LeetCode/1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ LeetCode/1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-
-        long long sum = 0;
-        int l = 0, ans = 1;
-
-        for (int r = 0; r < nums.size(); r++) {
-            sum += nums[r];
-
-            while ((long long)nums[r] * (r - l + 1) - sum > k) {
-                sum -= nums[l];
+    int maxFrequency(vector<int>& nums, int k) 
+    {
+        int l=0,r,n=nums.size(),result=0;
+        long long windowsum=0,currsum=0,ops,target;
+        sort(nums.begin(),nums.end());
+        for(r=0;r<n;r++)
+        {
+            target=nums[r];
+            currsum+=nums[r];
+            windowsum=target*(r-l+1);
+            ops=windowsum-currsum;
+            if(ops>k)
+            {
+                currsum-=nums[l];
                 l++;
+                ops=target*(r-l+1)-currsum;
             }
+            result=max(result,r-l+1);
 
-            ans = max(ans, r - l + 1);
         }
+        return result;
 
-        return ans;
+        
     }
 };
